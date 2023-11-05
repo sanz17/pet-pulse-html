0a5fdd347146551b92347b8cca7a13da9705e53d
sanz17 <kundusanhita13@gmail.com>
2023-11-05 23:24:23 +0530

    fix boxes

--- mqtt.js
+++ mqtt.js
@@ -7,8 +7,8 @@ function startConnectT() {
     document.getElementById("messages").innerHTML += "<span> Connecting... </span><br>"
     // document.getElementById("messages").innerHTML += "<span> Using the client Id " + clientID + " </span><br>";
     client = new Paho.Client(host, Number(port), clientID);
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
+    client.onConnectionLost = onConnectionLostT;
+    client.onMessageArrived = onMessageArrivedT;
     client.connect({
         // 'timeout': 10,
         // 'keepAliveInterval': 20,
@@ -24,11 +24,11 @@ function startConnectLat() {
     port = 8081;
     // userId  = document.getElementById("username").value;  
     // passwordId = document.getElementById("password").value;  
-    document.getElementById("messages").innerHTML += "<span> Connecting... </span><br>";
+    document.getElementById("messagesLat").innerHTML += "<span> Connecting... </span><br>";
     // document.getElementById("messages").innerHTML += "<span> Using the client Id " + clientID + " </span><br>";
     client = new Paho.Client(host, Number(port), clientID);
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
+    client.onConnectionLost = onConnectionLostLat;
+    client.onMessageArrived = onMessageArrivedLat;
     client.connect({
         // 'timeout': 10,
         // 'keepAliveInterval': 20,
@@ -44,11 +44,11 @@ function startConnectLong() {
     port = 8081;
     // userId  = document.getElementById("username").value;  
     // passwordId = document.getElementById("password").value;  
-    document.getElementById("messages").innerHTML += "<span> Connecting... </span><br>";
+    document.getElementById("messagesLong").innerHTML += "<span> Connecting... </span><br>";
     // document.getElementById("messages").innerHTML += "<span> Using the client Id " + clientID + " </span><br>";
     client = new Paho.Client(host, Number(port), clientID);
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
+    client.onConnectionLost = onConnectionLostLong;
+    client.onMessageArrived = onMessageArrivedLong;
     client.connect({
         // 'timeout': 10,
         // 'keepAliveInterval': 20,
@@ -64,11 +64,11 @@ function startConnectActivity() {
     port = 8081;
     // userId  = document.getElementById("username").value;  
     // passwordId = document.getElementById("password").value;  
-    document.getElementById("messages").innerHTML += "<span> Connecting... </span><br>";
+    document.getElementById("messagesMot").innerHTML += "<span> Connecting... </span><br>";
     // document.getElementById("messages").innerHTML += "<span> Using the client Id " + clientID + " </span><br>";
     client = new Paho.Client(host, Number(port), clientID);
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
+    client.onConnectionLost = onConnectionLostMot;
+    client.onMessageArrived = onMessageArrivedMot;
     client.connect({
         // 'timeout': 10,
         // 'keepAliveInterval': 20,
@@ -90,7 +90,7 @@ function onConnectT() {
 function onConnectLat() {
     topic = "pet123-lat"
 
-    document.getElementById("messages").innerHTML += "<span> Showing latitude... </span><br>";
+    document.getElementById("messagesLat").innerHTML += "<span> Showing latitude... </span><br>";
 
     client.subscribe(topic);
 }
@@ -98,7 +98,7 @@ function onConnectLat() {
 function onConnectLong() {
     topic = "pet123-long"
 
-    document.getElementById("messages").innerHTML += "<span> Showing longitude... </span><br>";
+    document.getElementById("messagesLong").innerHTML += "<span> Showing longitude... </span><br>";
 
     client.subscribe(topic);
 }
@@ -106,23 +106,60 @@ function onConnectLong() {
 function onConnectActivity() {
     topic = "pet123-mot"
 
-    document.getElementById("messages").innerHTML += "<span> Showing activity... </span><br>";
+    document.getElementById("messagesMot").innerHTML += "<span> Showing activity... </span><br>";
 
     client.subscribe(topic);
 }
 
 
 
-function onConnectionLost(responseObject) {
+function onConnectionLostT(responseObject) {
     document.getElementById("messages").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
     if (responseObject != 0) {
-        document.getElementById("messages").innerHTML += "<span> ERROR:" + responseObject.errorMessage + "</span><br>";
+        document.getElementById("messages").innerHTML += "<span> ERROR: " + responseObject.errorMessage + "</span><br>";
     }
 }
 
-function onMessageArrived(message) {
+function onMessageArrivedT(message) {
     console.log("OnMessageArrived: " + message.payloadString);
     document.getElementById("messages").innerHTML += "<span> Showing:" + message.destinationName + "| Message : " + message.payloadString + "</span><br>";
+}
+
+
+function onConnectionLostLat(responseObject) {
+    document.getElementById("messagesLat").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
+    if (responseObject != 0) {
+        document.getElementById("messagesLat").innerHTML += "<span> ERROR: " + responseObject.errorMessage + "</span><br>";
+    }
+}
+
+function onMessageArrivedLat(message) {
+    console.log("OnMessageArrived: " + message.payloadString);
+    document.getElementById("messagesLat").innerHTML += "<span> Showing:" + message.destinationName + "| Message : " + message.payloadString + "</span><br>";
+}
+
+function onConnectionLostLong(responseObject) {
+    document.getElementById("messagesLong").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
+    if (responseObject != 0) {
+        document.getElementById("messagesLong").innerHTML += "<span> ERROR: " + responseObject.errorMessage + "</span><br>";
+    }
+}
+
+function onMessageArrivedMot(message) {
+    console.log("OnMessageArrived: " + message.payloadString);
+    document.getElementById("messagesMot").innerHTML += "<span> Showing:" + message.destinationName + "| Message : " + message.payloadString + "</span><br>";
+}
+
+function onConnectionLostMot(responseObject) {
+    document.getElementById("messagesMot").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
+    if (responseObject != 0) {
+        document.getElementById("messagesMot").innerHTML += "<span> ERROR: " + responseObject.errorMessage + "</span><br>";
+    }
+}
+
+function onMessageArrivedLong(message) {
+    console.log("OnMessageArrived: " + message.payloadString);
+    document.getElementById("messagesLong").innerHTML += "<span> Showing:" + message.destinationName + "| Message : " + message.payloadString + "</span><br>";
 }
 
 function startDisconnect() {
@@ -133,12 +170,8 @@ function startDisconnect() {
 function publishMessage() {
     msg = document.getElementById("Message").value;
     topic = document.getElementById("topic_p").value;
-
     Message = new Paho.MQTT.Message(msg);
     Message.destinationName = topic;
-
     client.send(Message);
     document.getElementById("messages").innerHTML += "<span> Message to topic " + topic + " is sent </span><br>";
-
-
 }
